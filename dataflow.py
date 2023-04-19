9d98e437b76f652967110bd4f2c90522146c7e8d
Dan Herrera <whoahbot@bytewax.io>
2023-04-19 14:07:25 -0700

    Move back to stateful_map

--- dataflow.py
+++ dataflow.py
@@ -23,15 +23,9 @@ class RandomNumpyData(StatelessSource):
     def next(self):
         i, item = next(self._it)
         if i % 5 == 0:
-            return ("data", {"time": align_to + timedelta(seconds=i), "value": np.nan})
+            return ("data", np.nan)
         else:
-            return (
-                "data",
-                {
-                    "time": align_to + timedelta(seconds=i),
-                    "value": random.randrange(0, 10),
-                },
-            )
+            return ("data", random.randint(0, 10))
 
 
 class RandomNumpyInput(DynamicInput):
@@ -44,27 +38,33 @@ flow.input("input", RandomNumpyInput())
 # ('data', {'time': datetime.datetime(...), 'value': nan})
 
 
-def new_array():
-    return np.empty(0, dtype=object)
+class WindowedArray:
+    """Windowed Numpy Array.
+    Create a numpy array to run windowed statistics on.
+    """
+
+    def __init__(self, window_size):
+        self.last_n = np.empty(0, dtype=object)
+        self.n = window_size
+
+    def _push(self, value):
+        self.last_n = np.insert(self.last_n, 0, value)
+        try:
+            self.last_n = np.delete(self.last_n, self.n)
+        except IndexError:
+            pass
+
+    def impute_value(self, value):
+        self._push(value)
+        if np.isnan(value):
+            new_value = np.nanmean(self.last_n)
+        else:
+            new_value = value
+        return self, (value, new_value)
 
 
-def impute_value(acc, event):
-    new_value = event["value"]
-    if np.isnan(new_value):
-        new_value = np.nanmean(acc)
-
-    acc = np.insert(acc, 0, new_value)
-    return acc
-
-
-window_config = SlidingWindow(
-    length=timedelta(seconds=10), align_to=align_to, offset=timedelta(seconds=5)
+flow.stateful_map(
+    "windowed_array", lambda: WindowedArray(10), WindowedArray.impute_value
 )
-clock_config = EventClockConfig(
-    lambda e: e["time"], wait_for_system_duration=timedelta(seconds=0)
-)
-flow.fold_window(
-    "10_second_windows", clock_config, window_config, new_array, impute_value
-)
-
+# ("metric", (old value, new value))
 flow.output("out", StdOutput())
